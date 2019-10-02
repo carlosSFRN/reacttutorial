d13135217b593ddfc9d7b3f31de0c7d46d842c16
70235347418 <70235347418@NOTEBOOK-CARLOS>
2019-10-02 14:55:44 -0300

    inclusao routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './global.css';
+import Routes from './routes';
 import Header from './components/Header';
 import Main from './pages/main';
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
